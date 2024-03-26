67d84cecf24d53bcc14e85620b80405ce68c744c
fazerneo <35126921@student.murdoch.edu.au>
2024-03-26 23:46:41 +0800

    1

---
+++ ex1.py
@@ -0,0 +1,11 @@
+def average(a, b):
+    return (a + b) / 2
+
+a = float(input("please enter your first number: "))
+b = float(input("please enter your second number: "))
+
+a = int(a)
+b = int(b)
+
+Average = average(a,b)
+print(f"Average of {a} and {b} is {Average}")
